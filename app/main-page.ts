bf2037aed4ef4043446e68b2c0969c1a5bbdf3a0
vakrilov <alexander.vakrilov@telerik.com>
2016-07-04 14:07:26 +0300

    Update to 2.1.0

--- app/main-page.ts
+++ app/main-page.ts
@@ -1,28 +1,33 @@
-var abs = require("ui/layouts/absolute-layout");
-var utils = require("utils/utils");
+import * as utils from "utils/utils";
+import {Label} from "ui/label";
+import {View} from "ui/core/view";
+import {GestureEventData, PanGestureEventData, PinchGestureEventData} from "ui/gestures";
 
-var states = ["unknown", "start", "change", "end"];
+let states = ["unknown", "start", "change", "end"];
 
-var item;
-var statusLbl;
+let item: View;
+let statusLbl: Label;
 
-var density;
+let density: number;
+let prevDeltaX: number;
+let prevDeltaY: number;
+let startScale = 1;
 
-var prevDeltaX;
-var prevDeltaY;
-var startScale = 1
-
-function pageLoaded(args) {
-    var page = args.object;
+export function pageLoaded(args) {
+    const page = args.object;
     item = page.getViewById("item");
     statusLbl = page.getViewById("status");
     density = utils.layout.getDisplayDensity();
-    
+
+    item.translateX = 0;
+    item.translateY = 0;
+    item.scaleX = 1;
+    item.scaleY = 1;
+
     updateStatus();
 }
-exports.pageLoaded = pageLoaded;
 
-function onPan(args) {
+export function onPan(args: PanGestureEventData) {
     console.log("PAN[" + states[args.state] + "] deltaX: " + Math.round(args.deltaX) + " deltaY: " + Math.round(args.deltaY));
 
     if (args.state === 1) {
@@ -39,18 +44,16 @@ function onPan(args) {
 
     updateStatus();
 }
-exports.onPan = onPan;
 
-
-function onPinch(args) {
+export function onPinch(args: PinchGestureEventData) {
     console.log("PINCH[" + states[args.state] + "] scale: " + args.scale + " focusX: " + args.getFocusX() + " focusY: " + args.getFocusY());
 
     if (args.state === 1) {
-        var newOriginX = args.getFocusX() - item.translateX;
-        var newOriginY = args.getFocusY() - item.translateY;
+        const newOriginX = args.getFocusX() - item.translateX;
+        const newOriginY = args.getFocusY() - item.translateY;
 
-        var oldOriginX = item.originX * item.width;
-        var oldOriginY = item.originY * item.height;
+        const oldOriginX = item.originX * item.width;
+        const oldOriginY = item.originY * item.height;
 
         item.translateX += (oldOriginX - newOriginX) * (1 - item.scaleX);
         item.translateY += (oldOriginY - newOriginY) * (1 - item.scaleY);
@@ -62,8 +65,7 @@ function onPinch(args) {
     }
 
     else if (args.scale && args.scale !== 1) {
-        var newScale = startScale * args.scale;
-
+        let newScale = startScale * args.scale;
         newScale = Math.min(8, newScale);
         newScale = Math.max(0.125, newScale);
 
@@ -73,9 +75,8 @@ function onPinch(args) {
 
     updateStatus();
 }
-exports.onPinch = onPinch;
 
-function onDoubleTap(args) {
+export function onDoubleTap(args: GestureEventData) {
     console.log("DOUBLETAP");
 
     item.animate({
@@ -88,17 +89,12 @@ function onDoubleTap(args) {
     });
 
     updateStatus();
-
 }
-exports.onDoubleTap = onDoubleTap;
 
 function updateStatus() {
-
-    var text = "translate: [" + Math.round(item.translateX) + ", " + Math.round(item.translateY) + "]" +
+    const text = "translate: [" + Math.round(item.translateX) + ", " + Math.round(item.translateY) + "]" +
         "\nscale: [" + (Math.round(item.scaleX * 100) / 100) + ", " + (Math.round(item.scaleY * 100) / 100) + "]" +
-        "\norigin: [" + Math.round(item.originX * item.width) + ", " + Math.round(item.originY * item.height) + "]"
+        "\norigin: [" + Math.round(item.originX * item.width) + ", " + Math.round(item.originY * item.height) + "]";
 
     statusLbl.text = text;
-
-    //console.log(text + "");
 }
